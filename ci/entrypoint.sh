0ac2a415482b4206850bb94190cf4c094e81b3b5
Well <wellington.martins@uello.com.br>
2024-09-22 01:20:27 -0300

    feat: testing use cron

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -8,5 +8,14 @@ php artisan storage:link
 # Cache Configs, Events, Routes and Views
 php artisan optimize
 
+# Task Scheduling
+echo "* * * * * www-data /usr/local/bin/php /var/www/app/artisan schedule:run >> /dev/null 2>&1" > /etc/cron.d/laravel
+
+# Permission to crontab
+chmod 0644 /etc/cron.d/laravel
+
+# Start cron
+crontab /etc/cron.d/laravel
+
 # Start supervisord
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
