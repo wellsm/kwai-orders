ae4d55eb18c8182ba4a264b9afdab18222146f85
Well <wellington.martins@uello.com.br>
2024-09-22 01:13:58 -0300

    feat: ignore if database is not connected

--- routes/console.php
+++ routes/console.php
@@ -2,12 +2,19 @@
 
 use App\Console\Commands\ProfileSyncCommand;
 use App\Models\Team;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 
-Team::all()
-    ->each(function (Team $team) {
-        Schedule::command(ProfileSyncCommand::class, [$team->getUsername(), $team->getSyncAt()])
-            ->everyMinute()
-            ->appendOutputTo(storage_path('logs/profile-sync.log'))
-            ->withoutOverlapping();
-    });
+try {
+    DB::connection()->getPdo();
+
+    Team::all()
+        ->each(function (Team $team) {
+            Schedule::command(ProfileSyncCommand::class, [$team->getUsername(), $team->getSyncAt()])
+                ->everyMinute()
+                ->appendOutputTo(storage_path('logs/profile-sync.log'))
+                ->withoutOverlapping();
+        });
+} catch (Exception) {}
+
+
