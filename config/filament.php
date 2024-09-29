163b41a04470151246cd8377bf3d77f0a53bda13
Well <wellington.martins@uello.com.br>
2024-09-29 19:52:42 -0300

    feat: use forceTLS when https

--- config/filament.php
+++ config/filament.php
@@ -26,7 +26,7 @@ return [
             'authEndpoint' => '/broadcasting/auth',
             'disableStats' => true,
             'encrypted' => true,
-            'forceTLS' => false,
+            'forceTLS' => env('PUSHER_SCHEME', 'http') === 'https',
             'enabledTransports' => ['ws', 'wss']
         ],
 
